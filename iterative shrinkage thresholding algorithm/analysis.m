f6836568ede9d9e13fed658c2d2496fe5078c606
Leon Song <songliu0608@qq.com>
2019-11-12 17:17:55 +0800

    增加了一些CcIST、IST、CcStOMP的性能分析

--- iterative shrinkage thresholding algorithm/analysis.m
+++ iterative shrinkage thresholding algorithm/analysis.m
@@ -20,7 +20,7 @@ sig_laplacewavelet=exp(-(damp/sqrt(1-damp^2))*2*pi*f_vibrate*t).*sin(2*pi*f_vibr
 Wss=round(t0*fs);
 
 A1=1;                   %幅值
-A2=0.5;                 %幅值
+A2=1;                 %幅值
 
 sig=zeros(len,1);
 for k=1:len
@@ -85,54 +85,123 @@ Dic=Dic/norm(Dic);
 
 %% 比较多种算法重构效果图
 
-SNR=-6;
-[signal,noise]=noisegen(sig,SNR);
-figure();
-subplot(3,1,1);
-plot(t,sig);
-title('无噪仿真信号');
-xlabel('时间 t/s');
-ylabel('幅值 A(m/s^2)');
-ylim([-1,1]);
-subplot(3,1,2);
-plot(t,noise);
-title('噪声');
-xlabel('时间 t/s');
-ylabel('幅值 A(m/s^2)');
-ylim([-1,1]);
-subplot(3,1,3);
-plot(t,signal);
-title('仿真信号');
-xlabel('时间 t/s');
-ylabel('幅值 A(m/s^2)');
-ylim([-1,1]);
-
-% CcIST
-sigma=0.03;
-lamda=sigma*sqrt(2*log(cols));
-ts=0.7;
-theta_CcIST=ClusterShrinkIST(signal,Dic,lamda,20,ts);
-sig_CcIST=Dic*theta_CcIST;
-
-% IST
-sigma=0.03;
-lamda=sigma*sqrt(2*log(cols));
-theta_IST=ist(signal,Dic,lamda);
-sig_IST=Dic*theta_IST;
-
-% 作图
-figure();
-subplot(2,1,1);
-plot(t,sig_CcIST);
-title('CcIST');
-xlabel('时间 t/s');
-ylabel('幅值 A(m/s^2)');
+% SNR=-6;
+% [signal,noise]=noisegen(sig,SNR);
+% figure();
+% subplot(3,1,1);
+% plot(t,sig);
+% title('无噪仿真信号');
+% xlabel('时间 t/s');
+% ylabel('幅值 A(m/s^2)');
 % ylim([-1,1]);
-subplot(2,1,2);
-plot(t,sig_IST);
-title('IST');
-xlabel('时间 t/s');
-ylabel('幅值 A(m/s^2)');
+% subplot(3,1,2);
+% plot(t,noise);
+% title('噪声');
+% xlabel('时间 t/s');
+% ylabel('幅值 A(m/s^2)');
 % ylim([-1,1]);
-
+% subplot(3,1,3);
+% plot(t,signal);
+% title('仿真信号');
+% xlabel('时间 t/s');
+% ylabel('幅值 A(m/s^2)');
+% ylim([-1,1]);
+% 
+% % CcIST
+% sigma=0.02;
+% lamda=sigma*sqrt(2*log(cols));
+% ts=0.7;
+% distance=20;
+% theta_CcIST=ClusterShrinkIST(signal,Dic,lamda,distance,ts);
+% sig_CcIST=Dic*theta_CcIST;
+% 
+% % IST
+% sigma=0.02;
+% lamda=sigma*sqrt(2*log(cols));
+% theta_IST=ist(signal,Dic,lamda);
+% sig_IST=Dic*theta_IST;
+% 
+% % CcStOMP
+% ts=0.7;
+% distance=20;
+% maxIter=30;
+% [theta_CcStOMP,err_CcStOMP]=ClusterShrinkStOMP(signal,Dic,maxIter,ts,distance);
+% sig_CcStOMP=Dic*theta_CcStOMP;
+% 
+% % 作图
+% figure();
+% subplot(3,1,1);
+% plot(t,sig_CcIST);
+% title('CcIST');
+% xlabel('时间 t/s');
+% ylabel('幅值 A(m/s^2)');
+% % ylim([-1,1]);
+% subplot(3,1,2);
+% plot(t,sig_IST);
+% title('IST');
+% xlabel('时间 t/s');
+% ylabel('幅值 A(m/s^2)');
+% % ylim([-1,1]);
+% subplot(3,1,3);
+% plot(t,sig_CcStOMP);
+% title('CcStOMP');
+% xlabel('时间 t/s');
+% ylabel('幅值 A(m/s^2)');
+% % ylim([-1,1]);
 %% 比较多种信噪比下相关系数等指标的性能
+SNR_range=0:-0.5:-7;
+ex_num=50;% 每组实验次数
+
+cc=[];
+for SNR=SNR_range
+    [signal,noise]=noisegen(sig,SNR);
+    
+    temp=[];
+    for i=1:ex_num
+        
+        % CcIST
+        sigma=0.03;
+        lamda=sigma*sqrt(2*log(cols));
+        ts=0.7;
+        distance=20;
+        theta_CcIST=ClusterShrinkIST(signal,Dic,lamda,distance,ts);
+        sig_CcIST=Dic*theta_CcIST;
+
+        % IST
+        sigma=0.03;
+        lamda=sigma*sqrt(2*log(cols));
+        theta_IST=ist(signal,Dic,lamda);
+        sig_IST=Dic*theta_IST;
+
+        % CcStOMP
+        ts=0.7;
+        distance=20;
+        maxIter=30; 
+        [theta_CcStOMP,err_CcStOMP]=ClusterShrinkStOMP(signal,Dic,maxIter,ts,distance);
+        sig_CcStOMP=Dic*theta_CcStOMP;
+        
+        % 算相关度
+        r_CcIST=corrcoef(sig,sig_CcIST);
+        r_IST=corrcoef(sig,sig_IST);
+        r_CcStOMP=corrcoef(sig,sig_CcStOMP);
+        
+        temp=[temp;r_CcIST(1,2) r_IST(1,2) r_CcStOMP(1,2)];
+        
+    end
+    
+    cc=[cc;mean(temp)];
+    
+
+end
+
+figure()
+plot(SNR_range,cc(:,1),'xr-');
+hold on;
+plot(SNR_range,cc(:,2),'og-');
+plot(SNR_range,cc(:,3),'*b-');
+legend('CcIST','IST','CcStOMP');
+
+%%
+
+
+
